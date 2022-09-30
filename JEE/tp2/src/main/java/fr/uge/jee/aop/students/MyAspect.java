5e87229d91dc7930f43a02a685b43b83b2b1329c
CChevr <cedricus@msn.com>
2022-09-30 15:35:24 +0200

    JEE TP2 EX3 Q2

--- JEE/tp2/src/main/java/fr/uge/jee/aop/students/MyAspect.java
+++ JEE/tp2/src/main/java/fr/uge/jee/aop/students/MyAspect.java
@@ -1,14 +1,19 @@
 package fr.uge.jee.aop.students;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 @Aspect
 @Component
 public class MyAspect {
+    /*
     @Before("execution(* fr.uge.jee.aop.students.RegistrationService.create*(..))")
     public void beforeCreate(JoinPoint jp) throws Throwable {
         System.out.println("Before create");
@@ -17,5 +22,18 @@ public class MyAspect {
     @AfterReturning(value="execution(* fr.uge.jee.aop.students.RegistrationService.create*(..))", returning="value")
     public void afterCreate(JoinPoint jp, Object value) throws Throwable {
         System.out.println("After create");
+    }
+     */
+
+    @Around(value="execution(* fr.uge.jee.aop.students.RegistrationService.create*(..))")
+    public Object aferAdd(ProceedingJoinPoint pjp) throws Throwable {
+        var methodName = pjp.getSignature().getName();
+        var args = Arrays.toString(pjp.getArgs());
+
+        System.out.println("Calling "+methodName+" with arguments "+ args);
+        var ret = pjp.proceed(); // call the method
+        System.out.println("Return id "+ret+" by "+methodName);
+
+        return ret;
     }
 }
