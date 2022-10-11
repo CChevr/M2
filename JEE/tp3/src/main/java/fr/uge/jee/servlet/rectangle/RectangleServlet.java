4f1cdb22a1dbe721e3499bcd121c930d493eb114
CChevr <cedricus@msn.com>
2022-10-11 11:31:11 +0200

    JEE TP3 E2

--- JEE/tp3/src/main/java/fr/uge/jee/servlet/rectangle/RectangleServlet.java
+++ JEE/tp3/src/main/java/fr/uge/jee/servlet/rectangle/RectangleServlet.java
@@ -1,6 +1,5 @@
 package fr.uge.jee.servlet.rectangle;
 
-import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -9,6 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @WebServlet("/rectangle")
@@ -20,28 +20,73 @@ public class RectangleServlet extends HttpServlet {
         return lines.collect(Collectors.joining("\n"));
     }
 
+    private void emptyPage(PrintWriter writer) {
+        writer.format(this.page, "", "", "");
+    }
+
+    private void finalPage(PrintWriter writer, int height, int width) {
+        Objects.requireNonNull(writer);
+
+        writer.format("<p>Area of the rectangle is: %d</p>", (width * height));
+    }
+
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         if (this.page == null) {
             InputStream inputStream = getServletContext().getResourceAsStream("/WEB-INF/templates/rectangle-form.html");
             this.page = readFromInputStream(inputStream);
         }
+
         response.setContentType("text/html ; charset=utf-8");
         response.setCharacterEncoding("utf-8");
         PrintWriter writer = response.getWriter();
-        writer.println(this.page);
+        emptyPage(writer);
         writer.flush();
     }
 
-    private long computeResponse(HttpServletRequest request) throws IOException {
-        var body = request.getReader().readLine();
-        var length = body.split("&");
+    private void manageError(PrintWriter writer, String sHeight, String sWidth, String msg) {
+        Objects.requireNonNull(writer);
+        Objects.requireNonNull(sHeight);
+        Objects.requireNonNull(sWidth);
+        Objects.requireNonNull(msg);
 
-        if (length.length != 2) {
-            throw new IllegalArgumentException("Two length are necessaries");
+        writer.format(this.page, sHeight, sWidth, msg);
+    }
+
+    private void computeResponse(PrintWriter writer, HttpServletRequest request) {
+        Objects.requireNonNull(writer);
+        Objects.requireNonNull(request);
+        int height;
+        int width;
+
+        var sHeight = request.getParameter("height");
+        var sWidth = request.getParameter("width");
+
+        // Empty field
+        if (sHeight.equals("") || sWidth.equals("")) {
+            manageError(writer, sHeight, sWidth, "");
+            return;
         }
 
-        return 0;
+        // Not numbers
+        try {
+            height = Integer.parseInt(sHeight);
+            width = Integer.parseInt(sWidth);
+        } catch(NumberFormatException e) {
+            var msg = "Wrong format";
+            manageError(writer, sHeight, sWidth, msg);
+            return;
+        }
+
+        // Invalid numbers
+        if ((height < 0) || (width < 0)) {
+            var msg = "Fields must be positive";
+            manageError(writer, sHeight, sWidth, msg);
+            return;
+        }
+
+        // Correct entries
+        finalPage(writer, height, width);
     }
 
     @Override
@@ -49,9 +94,9 @@ public class RectangleServlet extends HttpServlet {
         response.setContentType("text/html ; charset=utf-8");
         response.setCharacterEncoding("utf-8");
         PrintWriter writer = response.getWriter();
-        var test = request.getReader();
-        var res = test.readLine();
-        writer.println("<!DOCTYPE html><html><p>"+res+"</p></html>");
+
+        computeResponse(writer, request);
+
         writer.flush();
     }
 }
