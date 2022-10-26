561469cb7ba88929848644d93850ba7d0fcc3492
CChevr <cedricus@msn.com>
2022-10-26 14:36:01 +0200

    2.2.2

--- BigData/td6/src/main/scala/td6.scala
+++ BigData/td6/src/main/scala/td6.scala
@@ -9,4 +9,8 @@ object td6 extends App {
   // 2.2.1
   val drugs = sc.textFile("med.txt").map(x => x.split("\t")).map(x => (x(0), x(1), x(6)))
   println("DRUGS : " + drugs.count())
+
+  // 2.2.2
+  val rep = drugs.map(x => (x._3, 1)).reduceByKey(_ + _)
+  rep.foreach(println)
 }
