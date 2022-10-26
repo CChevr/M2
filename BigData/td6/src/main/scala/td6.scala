6e3b574053bae05abbacb0cb4a78d6412df9837c
CChevr <cedricus@msn.com>
2022-10-26 15:04:38 +0200

    2.2.4

--- BigData/td6/src/main/scala/td6.scala
+++ BigData/td6/src/main/scala/td6.scala
@@ -15,9 +15,15 @@ object td6 extends App {
     val rep = drugs.map(x => (x._3, 1)).reduceByKey(_ + _)
     rep.foreach(println)                                // (Commercialis � e, 11861)\n  (Non commercialis � e, 1813)
     val comDrugs = drugs.filter(x => x._3 == "Commercialis�e").map(x => (x._1, x._2))
-    //println("COMDRUGS: " + comDrugs.count())          //COMDRUGS: 11861
+    //println("COMDRUGS: " + comDrugs.count())          // COMDRUGS: 11861
 
     // 2.2.3
     val sub = sc.textFile("compo.txt").map(x => x.split("\t")).map(x => (x(0).toLong, x(2), x(3), x(6)))
-    println("Compo count: " + sub.count())             // Compo count: 25484
+    println("Compo count: " + sub.count())              // Compo count: 25484
+
+    // 2.2.4
+    val resp2 = sub.map(x => (x._4, 1)).reduceByKey(_ + _)
+    resp2.foreach(println)                              // (FT,4463)\n    (SA, 21021)
+    val saSub = sub.filter(x => (x._4 == "SA")).map(x => (x._1, x._2, x._3))
+    println("saSub: " + saSub.count)                    // saSub: 21021
 }
