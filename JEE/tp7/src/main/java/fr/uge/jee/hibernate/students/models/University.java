6fb5611d6a112fea3451ad04fe2e2cb4081f1746
CChevr <cedricus@msn.com>
2022-11-11 09:42:41 +0100

    rename table

--- JEE/tp7/src/main/java/fr/uge/jee/hibernate/students/models/University.java
+++ JEE/tp7/src/main/java/fr/uge/jee/hibernate/students/models/University.java
@@ -3,7 +3,7 @@ package fr.uge.jee.hibernate.students.models;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "Univerties")
+@Table(name = "Universities")
 public class University {
     @Id
     @GeneratedValue
